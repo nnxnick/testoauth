472e3cb2d606019266a7c8a8dd4386bf3b0b2cb8
nnxnick <nnxnick@gmail.com>
2017-10-14 23:11:01 +0300

    Add changes

--- testapp/Global.asax.cs
+++ testapp/Global.asax.cs
@@ -13,8 +13,8 @@ namespace testapp
 		protected void Application_Start()
 		{
 			clientauth.globals.Init("http://oauth.naumenko.biz",
-				"1ec83b1fb3f4625e70d1e682672ea6a100c32c606064ca0f4aca748d3bd3d0f2",
-				"739f0c4a85e7433f938157e06b204257");
+				"c32c606064ca0f4aca748d3bd3d0f2739f0c4a85e7433f938157e06b204257",
+				"1ec83b1fb3f4625e70d1e682672ea6a100");
 			AreaRegistration.RegisterAllAreas();
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
